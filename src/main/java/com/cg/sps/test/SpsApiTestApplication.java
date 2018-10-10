2a77a936385fd17254c9397233eb10c8b6a3b2d1
Deepak <anjani.deepak@capgemini.com>
2018-10-10 19:36:23 +0530

    API Framework commited

---
+++ src/main/java/com/cg/sps/test/SpsApiTestApplication.java
@@ -0,0 +1,12 @@
+package com.cg.sps.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsApiTestApplication.class, args);
+	}
+}
